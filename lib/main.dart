3bed374fa92347beef95f15d01f9ce43d81dde6f
Yoshihiro Tanaka <contact@cordea.jp>
2021-10-10 13:26:46 +0900

    introduce flutter_redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
 import 'home.dart';
+import 'reducer.dart';
+import 'state.dart';
 
 void main() {
-  runApp(const MyApp());
+  final store = Store<AppState>(
+    reducer,
+    initialState: AppState(),
+    middleware: [],
+  );
+  runApp(MyApp(store: store));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.store}) : super(key: key);
+
+  final Store<AppState> store;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.amber),
-      home: const Home(),
+    return StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.amber),
+        home: const Home(),
+      ),
     );
   }
 }
