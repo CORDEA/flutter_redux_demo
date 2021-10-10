4f448cd410be10578c46853ac9672e949f46be75
Yoshihiro Tanaka <contact@cordea.jp>
2021-10-10 14:10:47 +0900

    update layout

--- lib/home.dart
+++ lib/home.dart
@@ -7,7 +7,35 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: const Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text('Url'),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Slider.adaptive(value: 0, onChanged: (_) {}),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Start'),
+                ),
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Stop'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
