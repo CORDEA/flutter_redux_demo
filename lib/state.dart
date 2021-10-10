51d69f01dfd360d77f7d3ab62d63c4bdfdc42476
Yoshihiro Tanaka <contact@cordea.jp>
2021-10-10 14:20:56 +0900

    introduce freezed

--- lib/state.dart
+++ lib/state.dart
@@ -1,1 +1,8 @@
-class AppState {}
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'state.freezed.dart';
+
+@freezed
+class AppState with _$AppState {
+  const factory AppState() = _AppState;
+}
